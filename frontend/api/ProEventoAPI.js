0d7b5cb034f6d23c36eefe1fa4ce697668a621e7
Frost Tianjian Xu <xutianjian@gmail.com>
2021-03-20 23:15:31 -0700

    Add react native environment and implement most APIs function calls

---
+++ frontend/api/ProEventoAPI.js
@@ -0,0 +1,220 @@
+import { PROEVENTO_BACKEND_SERVER } from "@env";
+
+const defaultOnSuccessCallback = data => {
+  console.log(data);
+};
+
+const defaultOnFailureCallback = error => {
+  console.warn(error);
+};
+
+export const testConnection = (
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/test`)
+    .then(response => response.text())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const login = (
+  body,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/account/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const register = (
+  body,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/account/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const changePassword = (
+  body,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/account/change_password`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.text())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const searchUserById = (
+  userId,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/user?` +
+      new URLSearchParams({
+        userId: userId,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const searchUsersByUsername = (
+  username,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/user/users_by_name?` +
+      new URLSearchParams({
+        username: username,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const getFollowers = (
+  userId,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/user/followers?` +
+      new URLSearchParams({
+        userId: userId,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const getFollowing = (
+  userId,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/user/following?` +
+      new URLSearchParams({
+        userId: userId,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const follow = (
+  body,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/user/follow`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.text())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const unfollow = (
+  body,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/user/unfollow`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.text())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const getAllTags = (
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/tag/all_tags`)
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const getAllEvents = (
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(`${PROEVENTO_BACKEND_SERVER}/api/event/all_events`)
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const searchEventById = (
+  eventId,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/event?` +
+      new URLSearchParams({
+        eventId: eventId,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
+
+export const searchEventsByName = (
+  name,
+  onSuccess = defaultOnSuccessCallback,
+  onFailure = defaultOnFailureCallback
+) => {
+  fetch(
+    `${PROEVENTO_BACKEND_SERVER}/api/event/events_by_name?` +
+      new URLSearchParams({
+        name: name,
+      })
+  )
+    .then(response => response.json())
+    .then(data => onSuccess(data))
+    .catch(error => onFailure(error));
+};
--- frontend/screen/TestScreen.js
+++ frontend/screen/TestScreen.js
@@ -1,10 +1,218 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  testConnection,
+  login,
+  register,
+  searchUserById,
+  searchUsersByUsername,
+  getFollowers,
+  getFollowing,
+  follow,
+  unfollow,
+  getAllTags,
+  getAllEvents,
+  searchEventById,
+  searchEventsByName,
+ } from "../api/ProEventoAPI";
 
 export default function TestScreen() {
+  const [testMsg, setTestMsg] = useState("");
+  const onTestConnection = () => {
+    testConnection(
+      data => {
+        console.log(data), setTestMsg(data);
+      },
+      err => {
+        console.log(err);
+        setTestMsg("Failed to connect to server.");
+      }
+    );
+  };
+
+  const onTestLogin = () => {
+    const testAccount = {
+      email: "tommy@usc.edu",
+      password: "uscfighton!",
+    };
+    login(
+      testAccount,
+      account => {
+        console.log(account), setTestMsg(account.user.username);
+      }
+    );
+  };
+
+  const onTestRegister = () => {
+    const testAccount = {
+      email: "shannon@mail.com",
+      password: "infotheory",
+      user: {
+        username: "Shannon",
+        biography: "A Mathematical Theorist of Cryptography.",
+        avatarUrl: "",
+        enableNotifications: true,
+        tags: [{ id: 4 }],
+      },
+    };
+    register(
+      testAccount,
+      account => {
+        console.log(account);
+        setTestMsg(account.user.username);
+      }
+    );
+  };
+
+  const onTestSearchUserById = () => {
+    searchUserById(
+      1,
+      user => {
+        if (user !== null) {
+          console.log(user);
+          setTestMsg(user.username);
+        }
+      },
+    );
+  }
+
+  const onTestSearchUsersByUsername = () => {
+    searchUsersByUsername(
+      "T",
+      userList => {
+        console.log(userList);
+        let msg = "";
+        userList.forEach(user => {
+          msg += user.username + "\n";
+        });
+        setTestMsg(msg);
+      },
+    );
+  }
+
+  const onTestGetFollowers = () => {
+    getFollowers(
+      1,
+      userList => {
+        console.log(userList);
+        let msg = "";
+        userList.forEach(user => {
+          msg += user.username + "\n";
+        });
+        setTestMsg(msg);
+      },
+    );
+  }
+
+  const onTestGetFollowing = () => {
+    getFollowing(
+      1,
+      userList => {
+        console.log(userList);
+        let msg = "";
+        userList.forEach(user => {
+          msg += user.username + "\n";
+        });
+        setTestMsg(msg);
+      },
+    );
+  }
+
+  const onTestFollow = () => {
+    const body = {
+      followerId: 1,
+      followeeId: 2,
+    };
+    follow(
+      body,
+      response => {
+        setTestMsg(response);
+      },
+    );
+  }
+
+  const onTestUnfollow = () => {
+    const body = {
+      followerId: 1,
+      followeeId: 2,
+    };
+    unfollow(
+      body,
+      response => {
+        setTestMsg(response);
+      },
+    );
+  }
+
+  const onTestGetAllTags = () => {
+    getAllTags(
+      tags => {
+        console.log(tags);
+        let msg = "";
+        tags.forEach(tag => {
+          msg += tag.name + '\n';
+        })
+        setTestMsg(msg);
+      }
+    );
+  }
+
+  const onTestGetAllEvents = () => {
+    getAllEvents(
+      eventList => {
+        console.log(eventList);
+        let msg = "";
+        eventList.forEach(event => {
+          msg += event.name + '\n';
+        })
+        setTestMsg(msg);
+      }
+    );
+  }
+
+  const onTestSearchEventById = () => {
+    searchEventById(
+      1,
+      event => {
+        if (event !== null) {
+          console.log(event);
+          setTestMsg(event.name);
+        }
+      },
+    );
+  }
+
+  const onTestSearchEventsByName = () => {
+    searchEventsByName(
+      "CSCI",
+      eventList => {
+        console.log(eventList);
+        let msg = "";
+        eventList.forEach(event => {
+          msg += event.name + "\n";
+        });
+        setTestMsg(msg);
+      },
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text>{testMsg}</Text>
+      <Button title="Test Connection" onPress={onTestConnection} />
+      <Button title="Test Login" onPress={onTestLogin} />
+      <Button title="Test Register (One-Timed)" onPress={onTestRegister} disabled />
+      <Button title="Test Search User 1" onPress={onTestSearchUserById} />
+      <Button title="Test Search Users 'T'" onPress={onTestSearchUsersByUsername} />
+      <Button title="Test Get User 1's Followers" onPress={onTestGetFollowers} />
+      <Button title="Test Get User 1's Following" onPress={onTestGetFollowing} />
+      <Button title="Test User 1 Follows User 2" onPress={onTestFollow} />
+      <Button title="Test User 1 Unfollows User 2" onPress={onTestUnfollow} />
+      <Button title="Test Get All Tags" onPress={onTestGetAllTags} />
+      <Button title="Test Get All Events" onPress={onTestGetAllEvents} />
+      <Button title="Test Search Event 1" onPress={onTestSearchEventById} />
+      <Button title="Test Search Event 'CSCI'" onPress={onTestSearchEventsByName} />
     </View>
   );
 }
