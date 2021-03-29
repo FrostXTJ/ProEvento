fe310206e8f78756cfb21498a329368dcac63e22
Frost Tianjian Xu <xutianjian@gmail.com>
2021-03-28 18:23:11 -0700

    Delete useless import statements.

--- backend/src/test/java/usc/cs310/ProEvento/service/UserServiceTest.java
+++ backend/src/test/java/usc/cs310/ProEvento/service/UserServiceTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import usc.cs310.ProEvento.model.User;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceTest {
